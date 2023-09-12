14e468ff75d3487486d08d520a5ccf973c5018a2
rawisha <rawvsmaw22@gmail.com>
2023-09-12 02:59:52 +0200

    invite link and manage server settings done

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { useEffect, useState } from "react";
+
+import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { InviteModal } from "@/components/modals/invite-modal";
+import { CreateServerModal } from "@/components/modals/create-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -17,6 +20,8 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />
+      <InviteModal />
+      <EditServerModal />
     </>
   );
 };
